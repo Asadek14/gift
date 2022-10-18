ef6cdcde3f12e490c662519ff69320d8d2b99df9
Asadek14 <94584241+Asadek14@users.noreply.github.com>
2022-10-17 22:44:04 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let kito = document.getElementById("personB")
+let kati = document.getElementById("personG")
+let heart = document.getElementById("heart")
+let number = 0
+
+const moveThem = () => {
+
+    if (number < 110)
+    {
+        number += 1
+        kito.style.right = number + "px"
+        setTimeout(moveThem, 40)
+    }
+}
+
+setTimeout(moveThem, 40)
+
+setTimeout(() => {heart.style.display = "block"}, 155 * 40)
+
